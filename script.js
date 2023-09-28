c624e9e6c6180b7d5b2c2516fcf17172edbff212
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-09-28 17:53:42 +0100

    fetch cards and display in a div

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const newDeckBtn = document.getElementById("new-deck-btn")
-//console.log(newDeckBtn)
+const drawNewBtn = document.getElementById("draw-new-btn")
+//console.log(drawNewBtn)
+
 const baseUrl = "https://apis.scrimba.com/deckofcards/api/deck/"
+
 // fetch data from api save the card deck id
 let deckId = ""
-const handleClick = () => {
 
+const getNewDeck = () => {
   fetch(baseUrl + "new/shuffle/", {method:"GET"})
-    .then(resp => resp.json())
-    .then(jsonData => {
-      deckId = jsonData.deck_id
-      console.log(deckId)
-    })
-  } 
+  .then(resp => resp.json())
+  .then(jsonData => {
+    deckId = jsonData.deck_id
+    console.log(deckId)
+  })
+} 
+getNewDeck()      // call fucntion to get a new deck when page loads
+newDeckBtn.addEventListener("click", getNewDeck)
 
-  newDeckBtn.addEventListener("click", handleClick)
+const drawNewCards = () => {
+  fetch(baseUrl + `${deckId}/draw/?count=2`)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data.cards[0].image)
+      return document.getElementById("cards-container").innerHTML = `
+      <div class="top-card" id="top-card">
+          <img src="${data.cards[1].image}" class="card" alt="Top playin card"/>
+      </div>
+      <div class="bottom-card" id="bottom-card">
+          <img src="${data.cards[0].image}" class="card" alt="Bottom playin card"/>
+      </div>`
+    })
+}
+drawNewBtn.addEventListener("click", drawNewCards)
