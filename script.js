2ae43e15e96d211a17965da9c12cd1e46379a86c
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-10-02 18:01:03 +0100

    scoring added

--- script.js
+++ script.js
@@ -3,39 +3,52 @@ const drawNewBtn = document.getElementById("draw-new-btn")
 const topCardEl = document.getElementById("top-card")
 const bottomCardEl = document.getElementById("bottom-card")
 const cardsContainerEl = document.getElementById("cards-container")
-//console.log(bottomCardEl)
+const cardsRemainingEl = document.getElementById("cards-remaining")
+const computerScoreEl = document.getElementById("comp-score")
+const playerScoreEl = document.getElementById("player-score")
+console.log(computerScoreEl)
 
 const baseUrl = "https://apis.scrimba.com/deckofcards/api/deck/"
 
-// fetch data from api save the card deck id
+
+let computerScore = 0
+let playerScore = 0
 let deckId = ""
 
 const getNewDeck = () => {
   fetch(baseUrl + "new/shuffle/", {method:"GET"})
   .then(resp => resp.json())
   .then(jsonData => {
+    cardsRemainingEl.innerText = `Remaining cards:${jsonData.remaining}`
     deckId = jsonData.deck_id
     console.log(deckId)
   })
 } 
-getNewDeck()      // call function to get a new deck when page loads
+//getNewDeck()      // call function to get a new deck when page loads
 newDeckBtn.addEventListener("click", getNewDeck)
 
 const drawNewCards = () => {
   fetch(baseUrl + `${deckId}/draw/?count=2`)
     .then(resp => resp.json())
     .then(data => { 
-      console.log(data.cards[0].image)
-      console.log(data.cards[1].image)
+  
       cardsContainerEl.innerHTML = `
+      
       <div class="top-card" id="top-card">
-        <img src="${data.cards[1].image}" class="card-img" alt="Top playin card"/>
+        <img src="${data.cards[0].image}" class="card-img" alt="Top playin card"/>
       </div>
+      <h3 class="cards-remaining">Remaining cards:${data.remaining}</h3>
       <div class="bottom-card" id="bottom-card">
-        <img src="${data.cards[0].image}" class="card-img" alt="Bottom playin card"/>
-      </div>`
+        <img src="${data.cards[1].image}" class="card-img" alt="Bottom playin card"/>
+      </div>
+      <h3 class="player-score score">Player: ${playerScore}</h3>`
+
       const winnerText = getWinningCard(data.cards[0], data.cards[1])
             document.getElementById("game-title").innerText =  winnerText
+      
+            if(data.remaining === 0){
+              drawNewBtn.disabled = true
+            }
     })
   }
   drawNewBtn.addEventListener("click", drawNewCards)
@@ -47,9 +60,13 @@ const drawNewCards = () => {
     const card2IndexValue = cardValuesArr.indexOf(card2.value)
 
     if(card1IndexValue > card2IndexValue){
+      computerScore++
+      computerScoreEl.innerText = `Computer: ${computerScore}`
       return "Card 1 Wins!"
     }
     else if(card1IndexValue < card2IndexValue){
+      playerScore++
+      playerScoreEl.innerText = `Player: ${playerScore}`
       return "Card 2 Wins!"
     }
     else{
@@ -57,11 +74,3 @@ const drawNewCards = () => {
     }
   }
 
-  // const card1Obj = {
-  //   value: "5"
-  // }
-  // const card2Obj = {
-  //   value: "2"
-  // }
-
-  // getWinningCard(card1Obj, card2Obj)
